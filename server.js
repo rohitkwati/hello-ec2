0348d9ae3db2d542525ca6c9b4c22099445f3ae4
rohitkwati <149990846+rohitkwati@users.noreply.github.com>
2025-11-14 21:26:20 +0530

    Create a basic HTTP server in server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer(function(req, res) {
+  const html = fs.readFileSync("index.html");
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.end(html);
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
